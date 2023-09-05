99690d9539169330cedb75a1b51af776043053b2
Chinh Lâm Điền <lamdienchinhtd9a2@gmail.com>
2023-09-05 15:54:58 +0700

    Modified Checkout page

--- src/pages/checkout/index.jsx
+++ src/pages/checkout/index.jsx
@@ -193,7 +193,7 @@ function Checkout(props) {
                             </div>
                         </div>
                         <div className="mx-[18px] mt-[20px]">
-                            <button className="w-[100%] text-[#FFF] flex flex-row gap-[18px] bg-primary-orange font-['Helvetica'] text-[18px] font-[700]">
+                            <button className="justify-center w-[100%] text-[#FFF] flex flex-row gap-[18px] bg-primary-orange font-['Helvetica'] text-[18px] font-[700]">
                                 Place an order
                                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
                                     <path d="M3.125 10H16.875" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
